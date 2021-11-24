edd41cdfe973aa3c9c5cffb8fe29627429d20942
Markus Punnar <markusp@ut.ee>
2021-11-24 22:59:03 +0200

    Create initial navbar for controlling the application

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import { useRecoilState } from "recoil";
 import { selectedAlgorithmAtom } from "../state/atoms";
 import { Algorithm } from "../types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import "../css/controls.scss";
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 interface ControlsProps {
   visualizeGrid: () => void;
@@ -15,35 +19,88 @@ interface ControlsProps {
 }
 
 const Controls = ({ visualizeGrid, resetGrid }: ControlsProps) => {
+  const [menuAnchorElement, setMenuAnchorElement] = React.useState(null);
+  const open = Boolean(menuAnchorElement);
   const [selectedAlgorithm, setSelectedAlgorithm] = useRecoilState(
     selectedAlgorithmAtom
   );
-  const handleAlgorithmChange = (event: SelectChangeEvent<string>) => {
-    setSelectedAlgorithm(event.target.value);
+  const handleAlgorithmChange = (event: any) => {
+    const { myValue: selectedAlgorithm } = event.currentTarget.dataset;
+    setSelectedAlgorithm(selectedAlgorithm);
+    setMenuAnchorElement(null);
+  };
+  const handleAlgorithmMenuClick = (event: any) => {
+    setMenuAnchorElement(event.currentTarget);
   };
   return (
-    <div>
-      <Box>
-        <FormControl fullWidth>
-          <InputLabel id="selectedAlgorithmLabel">Algorithm</InputLabel>
-          <Select
-            labelId="selectedAlgorithmLabel"
-            value={selectedAlgorithm}
-            label="Algorithm"
-            onChange={handleAlgorithmChange}
-            defaultValue={Algorithm.Dijkstra}
+    <div className="controls">
+      <AppBar
+        position="static"
+        sx={{
+          alignItems: "center",
+          mb: 10,
+          backgroundColor: "#247BA0",
+        }}
+      >
+        <Toolbar id="toolbar" sx={{ width: "100%" }}>
+          <Typography variant="h4" sx={{ ml: 3, flex: 1 }}>
+            Pathfinder
+          </Typography>
+          <Box>
+            <Button
+              sx={{ ml: 3 }}
+              color="inherit"
+              size="large"
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              endIcon={<BsFillCaretDownFill size="15" />}
+              onClick={handleAlgorithmMenuClick}
+            >
+              Algorithms
+            </Button>
+            <Menu
+              id="selectedAlgorithm"
+              open={open}
+              anchorEl={menuAnchorElement}
+              onClose={() => setMenuAnchorElement(null)}
+            >
+              <MenuItem
+                data-my-value={Algorithm.Dijkstra}
+                onClick={handleAlgorithmChange}
+              >
+                Dijkstra
+              </MenuItem>
+              <MenuItem
+                data-my-value={Algorithm.AStar}
+                onClick={handleAlgorithmChange}
+              >
+                A*
+              </MenuItem>
+            </Menu>
+            <Button
+              size="large"
+              color="inherit"
+              sx={{ ml: 3 }}
+              onClick={visualizeGrid}
+            >
+              {`Visualize ${selectedAlgorithm}`}
+            </Button>
+            <Button
+              size="large"
+              color="inherit"
+              sx={{ ml: 3 }}
+              onClick={resetGrid}
+            >
+              Reset grid
+            </Button>
+          </Box>
+          <Box
+            sx={{ mr: 3, flex: 1, display: "flex", justifyContent: "flex-end" }}
           >
-            <MenuItem value={Algorithm.Dijkstra}>Dijkstra</MenuItem>
-            <MenuItem value={Algorithm.AStar}>A*</MenuItem>
-          </Select>
-        </FormControl>
-        <Button sx={{ m: 5 }} variant="contained" onClick={visualizeGrid}>
-          Visualize!
-        </Button>
-        <Button sx={{ m: 5 }} variant="contained" onClick={resetGrid}>
-          Reset grid
-        </Button>
-      </Box>
+            <GiHamburgerMenu />
+          </Box>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
